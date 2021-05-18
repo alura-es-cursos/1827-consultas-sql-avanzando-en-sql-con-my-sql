c817c34258fe420e01e62d2fd3edc4a31012fadf
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-05-18 17:13:06 -0300

    Comandos Aula 2

--- comandos.sql
+++ comandos.sql
@@ -1,3 +1,8 @@
 /* En este archivo encontrarás todos los comandos ejecutados en SQL durante el desarrollo del curso de consultas SQL.
 
 Te invitamos a descargar e instalar MySQL de acuerdo con el video 1.2-Preparando el ambiente. */
+
+
+/* PROYECTO DEL AULA ANTERIOR 1 
+
+A partir de ahora, estás listo para ejecutar los comandos que instruiremos durante el Aula 2. ¡Te deseamos éxitos en tus estudios!*/
